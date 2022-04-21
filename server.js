84c0b762e8043fda504e28e0aa85192621e5d33a
jchen324 <jchen324@jh.edu>
2022-04-21 14:36:15 -0400

    separate static files

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 7000;
 
+app.use(express.static(path.join(__dirname, "app")));
+
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+   res.sendFile(path.join(__dirname, "app", "index.html"));
 });
 
 app.listen(port, () => {
